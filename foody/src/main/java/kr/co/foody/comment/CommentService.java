5dbe8f447f2c90b937a64975d671f5fa0b0ed38d
woong <fiveeyes11@gmail.com>
2022-08-30 20:11:46 +0900

    상세보기 포토리뷰 스와이퍼 - 종웅

--- foody/src/main/java/kr/co/foody/comment/CommentService.java
+++ foody/src/main/java/kr/co/foody/comment/CommentService.java
@@ -1,5 +1,6 @@
 package kr.co.foody.comment;
 
+import java.util.List;
 import java.util.Map;
 
 public interface CommentService {
@@ -10,5 +11,8 @@ public interface CommentService {
 	Map index(CommentVO vo);
 	Map wholeList(CommentVO vo);
 	int insert_reCmt(CommentVO vo);
+	
+	//게시글 번호로 사진 있는 리뷰만 리턴
+	List<CommentVO> selectPhotoReview(int no);
 	
 }
